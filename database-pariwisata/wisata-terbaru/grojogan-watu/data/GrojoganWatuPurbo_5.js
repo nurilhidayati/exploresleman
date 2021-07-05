9c54b091ec3687f896150bd7c2acbb7ac6079f9b
Nuril Hidayati <86280684+nurilhidayati@users.noreply.github.com>
2021-07-05 14:01:30 +0700

    Add files via upload

---
+++ database-pariwisata/wisata-terbaru/grojogan-watu/data/GrojoganWatuPurbo_5.js
@@ -0,0 +1,8 @@
+var json_GrojoganWatuPurbo_5 = {
+"type": "FeatureCollection",
+"name": "GrojoganWatuPurbo_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama Obyek Wisata": "Grojogan Watu Purbo", "Jenis Obyek Wisata": "Wisata Alam", "Alamat": "Bangunrejo, Merdikorejo, Kecamatan Tempel", "Daya Tarik Wisata": "Air terjun dan area spot foto yang instagramable", "Harga Tiket Masuk": "Belum dikenakan biaya tiket masuk, hanya dikenakan biaya  parkir Rp 5.000\/ motor dan Rp 10.000\/ mobil.", "Jam Operasional": "Setiap Hari (Pukul 06.00-18.00 WIB)", "Fasilitas": "Tempat parkir, toilet, tempat sampah, kios, dan mushola", "Aksesbilitas": "Akses jalan menuju lokasi objek wisata telah memadai, jalan sudah beraspal dan dapat dilalui kendaraan roda empat. Selanjutnya, perjalanan dilanjutkan dengan berjalan kaki menempuh jalan setapak untuk menuju ke lokasi air terjun.", "Pengelola": "Pokdarwis Bangunrejo", "Narahubung": "081327509611", "Foto": "C:\\foto\\obyek-wisata\\016.png", "Sumber Foto": "Instagram: @grojogan_watupurbo" }, "geometry": { "type": "Point", "coordinates": [ 110.33837, -7.634017 ] } }
+]
+}
