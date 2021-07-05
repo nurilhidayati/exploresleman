2f9e09bc8e4b6fc14b436dd8b0cbae03fc99b01c
Nuril Hidayati <86280684+nurilhidayati@users.noreply.github.com>
2021-07-05 22:38:52 +0700

    Add files via upload

---
+++ database-pariwisata/wisata-terbaru/illumina-glamping/data/IlluminaGlampingJogja_5.js
@@ -0,0 +1,8 @@
+var json_IlluminaGlampingJogja_5 = {
+"type": "FeatureCollection",
+"name": "IlluminaGlampingJogja_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama Obyek Wisata": "Illumina Glamping Jogja", "Jenis Obyek Wisata": "Wisata Alam", "Alamat": "Jl. Siaga, Kaliurang, Hargobinangun, Kecamatan Pakem", "Daya Tarik Wisata": "Berkemah di desa terdekat dengan Gunung Merapi yang dilengkapi fasilitas mewah (glamoour)", "Harga Tiket Masuk": "Paket yang ditawarkan di Glamping Indekostour mulai dari Rp.135.000 per orang. Ada pula pilihan paket VIP yang memiliki harga sebesar Rp.400.000 untuk satu tenda. Harga ini sudah termasuk sarapan serta makan malam dengan barbeque.", "Jam Operasional": "Setiap Hari", "Fasilitas": "Fasilitas yang disuguhkan oleh destinasi ini selain panorama juga berkemah dimalam hari ditemani gemerlap bintang dan keteduhan alam, serta menyediakan fasilitas tenda dan yang dilengkapi kasur empuk, pendingin ruangan, juga televisi layar datar", "Aksesbilitas": "Akses jalan menuju lokasi objek wisata telah memadai, jalan sudah beraspal dan dapat dilalui kendaraan roda empat", "Pengelola": "Indekostour", "Narahubung": "08179408899", "Foto": "C:\\foto\\obyek-wisata\\099.png", "Sumber Foto": "Instagram: @glamping_indekostour", "Petunjuk Arah": "<a href=\"https:\/\/www.google.com\/maps\/dir\/\/ILUMINA+GLAMPING+JOGJA,+Le+Jardin,+Jl+Tunggul+Arum,+Kaliurang,\" target=\"blank\">Lihat Rute<\/a>" }, "geometry": { "type": "Point", "coordinates": [ 110.426965, -7.596087 ] } }
+]
+}
