11f88a2d17b9a936959b54e8edfc839f174df659
Nuril Hidayati <86280684+nurilhidayati@users.noreply.github.com>
2021-07-05 14:07:42 +0700

    Add files via upload

---
+++ database-pariwisata/wisata-terbaru/obelix-hills/data/ObelixHills_5.js
@@ -0,0 +1,8 @@
+var json_ObelixHills_5 = {
+"type": "FeatureCollection",
+"name": "ObelixHills_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama Obyek Wisata": "Obelix Hills", "Jenis Obyek Wisata": "Wisata Alam", "Alamat": "Klumprit I, Klumprit II, Wukirharjo, Kecamatan Prambanan", "Daya Tarik Wisata": "Pemandangan alam di atas bukit dan panorama sunset di sore hari.", "Harga Tiket Masuk": "Rp 15.000 (Weekdays) dan Rp 20.000 (Weekend)", "Jam Operasional": "Senin s.d. Kamis (Pukul 10.00 - 21.00 WIB) | \nJum'at s.d. Minggu (Pukul 07.00 - 21.00 WIB)", "Fasilitas": "Tempat parkir, toilet umum, tempat sampah, kios, dan mushola", "Aksesbilitas": "Akses jalan menuju lokasi objek wisata cukup memadai, dengan jalan cor halus dikombinasi dengan jalan beraspal.", "Pengelola": "Pemerintah Desa Wukirharjo", "Narahubung": "-", "Foto": "C:\\foto\\obyek-wisata\\017.png", "Sumber Foto": "Instagram: @obelixhills" }, "geometry": { "type": "Point", "coordinates": [ 110.521571, -7.807032 ] } }
+]
+}
