a07ff2623afa29ebc340a536d8fe70f231cd5d86
Nuril Hidayati <86280684+nurilhidayati@users.noreply.github.com>
2021-07-05 14:11:56 +0700

    Add files via upload

---
+++ database-pariwisata/wisata-terbaru/restu-bumi/data/RestoeBoemiKreo_5.js
@@ -0,0 +1,8 @@
+var json_RestoeBoemiKreo_5 = {
+"type": "FeatureCollection",
+"name": "RestoeBoemiKreo_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama": "Restoe Boemi kreo", "Alamat": "Jl. Raya Banjararum, Kliran, Sendangagung, Kecamatan Minggir", "Deskripsi": "Restu Bumi Kreo dikonsep sebagai tempat kuliner yang menawarkan masakan tradisional khas Yogyakarta serta jajanan pasar sebagai camilan. Untuk menikmati makanan yang ada di Restu Bumi Kreo, sistemnya ambil sendiri atau prasmanan. Tapi ada beberapa menu y", "Menu": "Aneka masakan khas Indonesia", "Harga": "Mulai dari Rp.10.000\/ porsi", "Jam Operasional": "Weekdays  (Pukul 07.00-21.00  WIB) | Weekend  (Pukul 06:30 - 21:00 WIB)", "No. Telp": "081904221889", "Foto": "C:\\foto\\kuliner\\36.png", "Sumber Foto": "Instagram: @restubumikreo" }, "geometry": { "type": "Point", "coordinates": [ 110.231128, -7.724331 ] } }
+]
+}
