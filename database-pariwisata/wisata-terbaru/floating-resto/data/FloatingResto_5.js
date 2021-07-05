5221179333c919a131be37d29dfc43bc6c9ad657
Nuril Hidayati <86280684+nurilhidayati@users.noreply.github.com>
2021-07-05 13:59:17 +0700

    Add files via upload

---
+++ database-pariwisata/wisata-terbaru/floating-resto/data/FloatingResto_5.js
@@ -0,0 +1,8 @@
+var json_FloatingResto_5 = {
+"type": "FeatureCollection",
+"name": "FloatingResto_5",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Nama": "Floating Resto", "Alamat": "Jl. Roro Jonggrang Beran No. 8, Beran Kidul, Tridadi, Kecamatan Sleman", "Deskripsi": "Floating Resto merupakan salah satu rumah makan dengan konsep cafe mengapung di atas danau buatan. Pengunjung jika mau ke resto terapung di tengah embung harus menaiki dua dek terapung yang digerakkan dengan cara ditarik. Di sisi utara juga terdapat tiru", "Menu": "Aneka masakan seafood", "Harga": "mulai dari Rp.25.000", "Jam Operasional": "Setiap Hari (Pukul 12.00-20.00 WIB)", "No. Telp.": "081215829695", "Foto": "C:\\foto\\kuliner\\48.png", "Sumber Foto": "Instagram: @floatingrestojogja" }, "geometry": { "type": "Point", "coordinates": [ 110.357472, -7.720367 ] } }
+]
+}
