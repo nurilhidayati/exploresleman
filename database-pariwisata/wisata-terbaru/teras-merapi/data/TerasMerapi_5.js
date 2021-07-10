2c98ed78d01976d69520cc428925d71097db9db2
Nuril Hidayati <86280684+nurilhidayati@users.noreply.github.com>
2021-07-10 18:51:51 +0700

    Delete database-pariwisata/wisata-terbaru directory

--- database-pariwisata/wisata-terbaru/teras-merapi/data/TerasMerapi_5.js
+++
@@ -1,8 +0,0 @@
-var json_TerasMerapi_5 = {
-"type": "FeatureCollection",
-"name": "TerasMerapi_5",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "Nama Obyek Wisata": "Teras Merapi", "Jenis Obyek Wisata": "Wisata Alam", "Alamat": "Dusun Kalitengah Kidul, Glagaharjo, Kecamatan Cangkringan", "Daya Tarik Wisata": "Pemandangan Gunung Merapi dari dekat, camp ground, dan area spot foto yang instagramable", "Harga Tiket Masuk": "Rp.10.000 (Dewasa), Rp. 5.000 (Anak-anak), atau Rp. 15.000 (tiket camping)", "Jam Operasional": "Setiap Hari (Pukul 08.00-19.00 WIB)", "Fasilitas": "Tempat parkir, warung makan,  toilet, dan kolam renang", "Aksesbilitas": "Akses jalan menuju lokasi objek wisata telah memadai, jalan sudah beraspal dan dapat dilalui kendaraan roda empat", "Pengelola": "Pemerintah Dusun Kalitengah Kidul", "Narahubung": "-", "Foto": "C:\\foto\\obyek-wisata\\108.png", "Sumber Foto": "Instagram: @wisata.terasmerapi", "Petunjuk Arah": "<a href=\"https:\/\/www.google.com\/maps\/dir\/\/Obelix+Hills+Sunset+View,+Klumprit+II,\" target=\"blank\">Lihat Rute<\/a>" }, "geometry": { "type": "Point", "coordinates": [ 110.455877, -7.58769 ] } }
-]
-}
